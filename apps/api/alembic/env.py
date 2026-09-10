cf7c151591d80910f8d8a2366f7b6e83cc3f0232
marituMegersa <marituMegersa@users.noreply.github.com>
2026-09-10 20:01:30 +0300

    Feature: Add Alembic migration environment setup & alembic.ini configuration

--- apps/api/alembic/env.py
+++ apps/api/alembic/env.py
@@ -1,15 +1,8 @@
 from alembic import context
 from sqlalchemy import engine_from_config, pool
-import os
 
 config = context.config
 target_metadata = None
-
-def run_migrations_offline():
-    url = config.get_main_option("sqlalchemy.url")
-    context.configure(url=url, target_metadata=target_metadata, literal_binds=True)
-    with context.begin_transaction():
-        context.run_migrations()
 
 def run_migrations_online():
     connectable = engine_from_config(config.get_section(config.config_ini_section), poolclass=pool.NullPool)
@@ -18,7 +11,4 @@ def run_migrations_online():
         with context.begin_transaction():
             context.run_migrations()
 
-if context.is_offline_mode():
-    run_migrations_offline()
-else:
-    run_migrations_online()
+run_migrations_online()
--- apps/api/alembic/script.py.mako
+++ apps/api/alembic/script.py.mako
@@ -1,10 +1,4 @@
-"""${message}
-
-Revision ID: ${up_revision}
-Revises: ${down_revision | repr, empty}
-Create Date: ${create_date}
-
-"""
+"""${message}"""
 from alembic import op
 import sqlalchemy as sa
 
